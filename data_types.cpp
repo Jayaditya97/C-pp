37e27980675273f36d68b24ac3ea3b5bb2959c7c
Jayaditya Nagpal <jayadityanagpal@gmail.com>
2025-07-03 12:43:35 +0530

    data_types

---
+++ data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a = 10;
+    float b = 12.444;
+    double c = 2324.34243;
+    char d = 'A';
+
+    std::cout << "int: " << a << "\n";
+    std::cout << "float: " << b << "\n";
+    std::cout << "double: " << c << "\n";
+    std::cout << "char: " << d << "\n";
+
+    return 0;
+}
